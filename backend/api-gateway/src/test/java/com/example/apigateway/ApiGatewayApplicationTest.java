94bd8f99029a6e11e072a199f73074c998a51453
Anna Martirosyan <anna.martirosyan.external@worldline.com>
2025-04-17 22:23:49 +0400

    Added api-gateway

---
+++ backend/api-gateway/src/test/java/com/example/apigateway/ApiGatewayApplicationTest.java
@@ -0,0 +1,12 @@
+package com.example.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApiGatewayApplicationTest {
+    @Test
+    public void contextLoads() {
+        // This will fail if the application context cannot start.
+    }
+}
